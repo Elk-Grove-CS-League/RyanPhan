7bb29b692ea2ff08ca0b82c9bb299f325d3246bd
ryanphan07 <94889760+ryanphan07@users.noreply.github.com>
2022-06-20 18:18:03 +0000

    origin.js

---
+++ index.js
@@ -0,0 +1,12 @@
+
+var age = 70;
+
+if (age >= 60) {
+    console.log("senior")
+} else if (age >= 18) {
+    console.log("adult")
+} else if (age >=13){
+    console.log("teen")
+} else {
+    console.log("child")
+}
