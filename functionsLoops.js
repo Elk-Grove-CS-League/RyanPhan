6c5f9bc4faaf93d1745d94c1791eebf80e1d0421
ryanphan07 <94889760+ryanphan07@users.noreply.github.com>
2022-06-21 18:00:48 +0000

    originJsFunctionsLoops

---
+++ functionsLoops.js
@@ -0,0 +1,107 @@
+/*
+function minecraft(hunger){
+    if (hunger > 10 || hunger < 0){
+        console.log("Are you sure you put in the correct amount of hunger?");
+    } else if (hunger >= 7.5 && hunger <= 10 ){
+        console.log("You can definitely explore the cave!");
+        hunger = hunger - 5;
+    } else if (hunger >= 5){
+        console.log("You can explore a bit.");
+        hunger = hunger - 3;
+    } else if (hunger >= 3){
+        console.log("Run if you see mob");
+        hunger = hunger  - 1;
+    } else if (hunger > 0){
+        console.log("Definitely do not explore");
+    } else {
+        console.log("I think you're dead.");
+    } 
+    return(hunger);
+}
+
+//hunger = minecraft(10);
+//console.log("You now have " + hunger + " hunger.");
+
+
+
+function greet(firstName) {
+
+    return("Hello " + firstName + ". ");
+ }
+
+function scolding(greeting, time){
+    if (time >= 12){
+    return("Why are you home so late?! I know you're an idol, but this is ridiculous.");
+    } else {
+        return(greeting);
+    }
+}
+let greeting = greet("Suga");
+greeting = greeting + scolding(greeting, 13);
+console.log(greeting);
+*/
+
+
+/*
+
+let number = [1, 2, 3, 4, 5]
+let names = ["Bob", "Bobby", "Bobette"]
+/*
+for(let j of number){
+    
+}
+*/
+/*
+for (let element in names){
+    console.log(`${element}, want to go to the store with me?`);
+}
+
+for (let element of names){
+    console.log(`${element}, want to go to the store with me?`);
+}
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+const prompt = require("prompt-sync")({siginit: true});
+
+//let name = prompt("What is your name? ")
+//console.log("Hello " + name)
+
+
+
+/*
+num = []
+for (let i = 0; i < 10; i++){
+    let number = prompt("Enter a number ");
+    num[i] = number;
+}
+console.log(num);
+
+console.log(num.length);
+
+*/
+
+
+
+
+
+
+let numbers = [4, 6, 1, 3, 1, 3, 4, 5, 6, 9];
+
+//console.log(numbers.sort());
+
+//console.log(typeof(numbers))
+
+let name = true
+console.log(name);
